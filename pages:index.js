edbb15212f2b184ecf7144e172c5e700558b442f
hershrmalik11-hue <hershrmalik11@gmail.com>
2025-09-08 21:00:47 -0400

    Add files via upload

---
+++ pages:index.js
@@ -0,0 +1,9 @@
+import React, { useState, useEffect } from 'react';
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer, LineChart, Line } from 'recharts';
+import { TrendingUp, TrendingDown, Shield, Clock, DollarSign, Users, AlertTriangle, CheckCircle, Target, Zap } from 'lucide-react';
+
+// [Copy the entire ComplianceImpactSimulator component code from the artifact here]
+
+export default function Home() {
+  return <ComplianceImpactSimulator />;
+}
